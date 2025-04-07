c6a286d3719e0e461ae8ffdcf93d26264ea7371a
ABIALLURI <166591811+ABIALLURI@users.noreply.github.com>
2025-04-07 22:34:31 +0530

    commit

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Toggle password visibility
-document.querySelector('.toggle-password').addEventListener('click', function() {
-    const password = document.getElementById('password');
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    this.classList.toggle('fa-eye');
-    this.classList.toggle('fa-eye-slash');
-});
